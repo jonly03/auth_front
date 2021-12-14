c7cfbc289d9ad07c86baa1b931ea725b1cf3b548
Nelly Sugu <jonly03@gmail.com>
2021-12-13 22:29:25 -0500

    authorize then userinfo

--- script.js
+++ script.js
@@ -1,51 +1,65 @@
 const authButton = document.querySelector("#auth");
 const API_BASE_URL = "https://alan-auth.herokuapp.com";
 const LOCAL_STORAGE_AUTH_ITEM = "authEmail";
+const LOGIN_TEXT = "Login";
+const LOGOUT_TEXT = "Logout";
 
 authButton.addEventListener("click", handleAuth);
 function handleAuth() {
-  const id = localStorage.getItem(LOCAL_STORAGE_AUTH_ITEM);
+  const email = localStorage.getItem(LOCAL_STORAGE_AUTH_ITEM);
 
-  if (!id) {
-    // window.location =
-    //   "https://dev-v8yhc0o7.us.auth0.com/authorize?response_type=token&client_id=tM4rxIGpbjNLFjuWxNcojBsOCy1hIQzP&redirect_uri=https://nellysugu.com/auth_front/";
-    fetch(`${API_BASE_URL}/myLogin`)
-      .then((res) => res.json())
-      .then(({ isAuthenticated, user }) => {
-        if (!isAuthenticated) {
-          authButton.innerText = "Login";
-          console.log("Failed to log in");
-        } else {
-          authButton.innerText = "Logout";
-          console.log(user);
-        }
-      });
+  if (!email) {
+    window.location =
+      "https://dev-v8yhc0o7.us.auth0.com/authorize?response_type=token&client_id=tM4rxIGpbjNLFjuWxNcojBsOCy1hIQzP&redirect_uri=https://www.nellysugu.com/auth_front/";
   } else {
     // user attempting to logout
     // clear the localstorage and call our logout endpoint
     fetch(`${API_BASE_URL}/logout`).then((res) => {
       localStorage.removeItem(LOCAL_STORAGE_AUTH_ITEM);
+      toggleAuthButton();
     });
   }
 }
 
-// on load, check if user is authenticated
-// when user is authenticated, store their email in our localstorage
-// on load, check if there is an email in our localstorage
-// if no email in our local storage, force user to login again
-function checkAuth() {
-  const email = localStorage.getItem(LOCAL_STORAGE_AUTH_ITEM);
-
-  fetch(`${API_BASE_URL}/loggedIn`)
-    .then((res) => res.json())
-    .then(({ isAuthenticated, user }) => {
-      if (!isAuthenticated) {
-        authButton.innerText = "Login";
-      } else {
-        authButton.innerText = "Logout";
-        console.log(user);
-      }
-    });
+function toggleAuthButton() {
+  const authButton = document.getElementById("auth");
+  if (authButton.innerText === LOGIN_TEXT) {
+    authButton.innerText = LOGOUT_TEXT;
+  } else {
+    authButton.innerText = LOGIN_TEXT;
+  }
 }
 
+// on load, check user is authenticated
+// we are authenticated when the user got to from a url with an accesstoken
+// grab the access token, send it to our backend to get the user profile
+// when we get the userProfile, store the email in our localstorage
 checkAuth();
+function checkAuth() {
+  const url = window.location.href;
+  if (!url.includes("#access_token")) {
+    // not authenticate
+    // check if there is an email in our localstorage
+    const email = localStorage.getItem(LOCAL_STORAGE_AUTH_ITEM);
+    const authButton = document.getElementById("auth");
+
+    if (email && authButton.innerText === LOGIN_TEXT) {
+      authButton.innerText = LOGOUT_TEXT;
+    } else if (!email && authButton.innerText === LOGOUT_TEXT) {
+      authButton.innerText = LOGIN_TEXT;
+    }
+
+    return;
+  }
+
+  const startIdx = url.indexOf("=") + 1;
+  const endIdx = url.indexOf("&");
+  const access_token = url.substring(startIdx, endIdx);
+
+  fetch(`localhost:3000/userinfo?accessToken=${access_token}`)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => {
+      console.log(error);
+    });
+}
